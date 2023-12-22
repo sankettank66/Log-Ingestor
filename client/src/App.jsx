402a818f0daf251a2b5ce87e4d744bcb8cb7f615
sanket tank <sankettank66@gmail.com>
2023-12-22 20:32:50 +0530

    Initial commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,12 +6,19 @@ import Home from './pages/Home'
 import Logs from './pages/Logs'
 
 function App() {
-
+  const PAGENOTFOUND = () => {
+    return (
+      <div className="flex justify-center items-center">
+        <h1 className="text-6xl">404 Page not found</h1>
+      </div>
+    );
+  }
   return (
     <>
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/logs' element={<Logs />} />
+        <Route path='*' element={<PAGENOTFOUND />} />
       </Routes>
     </>
   );
